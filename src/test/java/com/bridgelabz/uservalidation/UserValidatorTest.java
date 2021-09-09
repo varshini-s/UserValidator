30b8c24963aa8761ed9e112adcb3b1c26a11a1ea
varshini <dreamvarshini@gmail.com>
2021-09-09 20:50:53 +0530

    ADDED: test case to check for valid first name

---
+++ src/test/java/com/bridgelabz/uservalidation/UserValidatorTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.uservalidation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserValidatorTest 
+{
+
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() 
+	{
+			
+		UserValidator userValidator = new UserValidator();
+		boolean isValid=userValidator.validateName("Emma");
+		Assert.assertTrue(isValid);
+		
+	}
+
+	
+}
