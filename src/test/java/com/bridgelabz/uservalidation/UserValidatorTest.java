417b929613328d7f82cb605e2039a3fe78172c6f
varshini <dreamvarshini@gmail.com>
2021-09-09 20:57:39 +0530

    ADDED: test case to return true for valid last name

--- src/test/java/com/bridgelabz/uservalidation/UserValidatorTest.java
+++ src/test/java/com/bridgelabz/uservalidation/UserValidatorTest.java
@@ -53,6 +53,15 @@ public class UserValidatorTest
 		Assert.assertFalse(isValid);
 
 	}
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() 
+	{
+			
+		UserValidator userValidator = new UserValidator();
+		boolean isValid=userValidator.validateName("Wilson");
+		Assert.assertTrue(isValid);
+		
+	}
 	
 
 	
