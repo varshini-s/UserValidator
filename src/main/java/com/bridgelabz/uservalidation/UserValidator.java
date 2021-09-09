1c60737f94de09c90965f8b2ff7982cef8c6363a
varshini <dreamvarshini@gmail.com>
2021-09-09 20:42:47 +0530

    MODIFIED: pattern changed to include special character

--- src/main/java/com/bridgelabz/uservalidation/UserValidator.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidator.java
@@ -8,8 +8,7 @@ public class UserValidator
 	private static final String NAME_PATTERN="^[A-Z][a-z]{2,}$";
 	private static final String EMAIL_ID_PATTERN="^[a-z0-9]+([+[.]_-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}$";
-	private static final String PASSWORD_PATERN="^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)$";
-	
+	private static final String PASSWORD_PATERN="^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)[0-9a-zA-Z]*[@#$%][0-9a-zA-Z]*$";	
 	
 	public boolean validateName(String name)
 	{
