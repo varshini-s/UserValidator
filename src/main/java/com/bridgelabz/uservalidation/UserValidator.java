cdb6713df3bef4f153411109a2fc81175afc9ec9
varshini <dreamvarshini@gmail.com>
2021-09-09 20:39:10 +0530

    ADDED: function to validate Password

--- src/main/java/com/bridgelabz/uservalidation/UserValidator.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidator.java
@@ -8,6 +8,7 @@ public class UserValidator
 	private static final String NAME_PATTERN="^[A-Z][a-z]{2,}$";
 	private static final String EMAIL_ID_PATTERN="^[a-z0-9]+([+[.]_-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}$";
+	private static final String PASSWORD_PATERN="^(?=.{8,}$)$";
 	
 	
 	public boolean validateName(String name)
@@ -25,7 +26,11 @@ public class UserValidator
 		Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
 		return pattern.matcher(phoneNumber).matches();
 	}
-	
+	public boolean validatePassword(String passWord)
+	{
+		Pattern pattern = Pattern.compile(PASSWORD_PATERN);
+		return pattern.matcher(passWord).matches();
+	}
 	
 	
 }
