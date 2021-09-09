c752e8a4e7f2ee4fddf18d6db46096a2cd7d8fef
varshini <dreamvarshini@gmail.com>
2021-09-09 21:58:35 +0530

    REFACTORED: try catch block for validate name function

---
+++ src/main/java/com/bridgelabz/uservalidation/UserValidatorException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.uservalidation;
+
+public class UserValidatorException extends Exception
+{
+
+	public UserValidatorException(String message) 
+	
+	{
+		super(message);
+	}
+	
+}
