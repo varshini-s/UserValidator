40d235f6ad093bea8c4a6d58c5ed31d27ff8f6a0
varshini <dreamvarshini@gmail.com>
2021-09-11 15:43:24 +0530

    MODIFIED: custome exception class changed to extend runtime exception

--- src/main/java/com/bridgelabz/uservalidation/UserValidatorException.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidatorException.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.uservalidation;
 
-public class UserValidatorException extends Exception
+public class UserValidatorException extends RuntimeException
 {
 
 	enum ExceptionType 
