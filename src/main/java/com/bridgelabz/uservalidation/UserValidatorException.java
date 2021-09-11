0666ab046dc4241c87161ddebc147e73b9502580
varshini <dreamvarshini@gmail.com>
2021-09-11 15:25:12 +0530

    REFACTORED: added enum declaration for custom exception

--- src/main/java/com/bridgelabz/uservalidation/UserValidatorException.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidatorException.java
@@ -3,10 +3,19 @@ package com.bridgelabz.uservalidation;
 public class UserValidatorException extends Exception
 {
 
-	public UserValidatorException(String message) 
-	
+	enum ExceptionType 
 	{
-		super(message);
+		ENTERED_NULL, ENTERED_EMPTY
 	}
-	
+
+	ExceptionType type;
+
+	public UserValidatorException(ExceptionType type, String message) 
+	{
+
+		super(message);
+		this.type = type;
+
+	}
+
 }
