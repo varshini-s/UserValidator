f19d4ed7e6d8aec6bceaa20d30b4636d0849db40
varshini <dreamvarshini@gmail.com>
2021-09-13 22:35:25 +0530

    REFACTORED: validate name function to use lambda function

--- src/main/java/com/bridgelabz/uservalidation/UserValidator.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidator.java
@@ -11,31 +11,39 @@ public class UserValidator
 	private static final String EMAIL_ID_PATTERN="^[a-z0-9]+([+[.]_-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}$";
 	private static final String PASSWORD_PATERN="^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)[0-9a-zA-Z]*[@#$%][0-9a-zA-Z]*$";	
-
-	public boolean validateName(String name) throws UserValidatorException 
+	
+	@FunctionalInterface
+	interface ValidationIF
 	{
-		try
+		boolean validate(String stringToValidate);
+		
+		
+	}
+	
+	ValidationIF validateName = name->{try
+	{
+		if(name.isEmpty())
 		{
-			if(name.isEmpty())
-			{
-				throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid Name");
-			}
-			else
-			{		
-				Pattern pattern = Pattern.compile(NAME_PATTERN);
-				return pattern.matcher(name).matches();
-
-			}
-
-
+			throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid Name");
+		}
+		else
+		{		
+			Pattern pattern = Pattern.compile(NAME_PATTERN);
+			return pattern.matcher(name).matches();
 
 		}
-		catch (NullPointerException e) 
-		{
-			throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid Name");
-		}
+
+
 
 	}
+	catch (NullPointerException e) 
+	{
+		throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid Name");
+	}};
+	
+	
+
+	
 	public boolean validateEMail(String eMailID) throws UserValidatorException
 	{
 		try
