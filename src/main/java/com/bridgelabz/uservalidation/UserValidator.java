8b44dbe584520d14159fe06fe3756264cedde2c6
varshini <dreamvarshini@gmail.com>
2021-09-13 22:46:03 +0530

    REFACTORED: validate phone number function to use lambda function

--- src/main/java/com/bridgelabz/uservalidation/UserValidator.java
+++ src/main/java/com/bridgelabz/uservalidation/UserValidator.java
@@ -11,63 +11,59 @@ public class UserValidator
 	private static final String EMAIL_ID_PATTERN="^[a-z0-9]+([+[.]_-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}$";
 	private static final String PASSWORD_PATERN="^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)[0-9a-zA-Z]*[@#$%][0-9a-zA-Z]*$";	
-	
+
 	@FunctionalInterface
 	interface ValidationIF
 	{
-		boolean validate(String stringToValidate);
-		
-		
+		boolean validate(String stringToValidate) throws UserValidatorException;
+
+
 	}
-	
+
 	ValidationIF validateName = name->{try
-	{
-		if(name.isEmpty())
 		{
-			throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid Name");
-		}
-		else
-		{		
-			Pattern pattern = Pattern.compile(NAME_PATTERN);
-			return pattern.matcher(name).matches();
-
-		}
-
-
-
-	}
-	catch (NullPointerException e) 
-	{
-		throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid Name");
-	}};
+			if(name.isEmpty())
+			{
+				throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid Name");
+			}
+			else
+			{		
+				Pattern pattern = Pattern.compile(NAME_PATTERN);
+				return pattern.matcher(name).matches();
 	
-	
+			}
+
+		}
+		catch (NullPointerException e) 
+		{
+			throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid Name");
+		}
+	};
+
+
 	ValidationIF validateEMail= eMailID->{try
-	{
-		if(eMailID.isEmpty())
 		{
-			throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid mail");
-		}
-		else
-		{		
-			Pattern pattern = Pattern.compile(EMAIL_ID_PATTERN);
-			return pattern.matcher(eMailID).matches();
-
-		}
-
-
-	}
-	catch (NullPointerException e) 
-	{
-		throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid mail");
-	}
-};
-
+			if(eMailID.isEmpty())
+			{
+				throw new UserValidatorException(ExceptionType.ENTERED_EMPTY,"Please enter valid mail");
+			}
+			else
+			{		
+				Pattern pattern = Pattern.compile(EMAIL_ID_PATTERN);
+				return pattern.matcher(eMailID).matches();
 	
+			}
+	
+	
+		}
+		catch (NullPointerException e) 
+		{
+			throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid mail");
+		}
+	};
 
-	public boolean validatePhoneNumber(String phoneNumber) throws UserValidatorException
-	{
-		try
+
+	ValidationIF validatePhoneNumber=phoneNumber->{try
 		{
 			if(phoneNumber.isEmpty())
 			{
@@ -77,17 +73,18 @@ public class UserValidator
 			{		
 				Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
 				return pattern.matcher(phoneNumber).matches();
-
+	
 			}
-
 
 		}
 		catch (NullPointerException e) 
 		{
 			throw new UserValidatorException(ExceptionType.ENTERED_NULL,"Please enter valid phone number");
 		}
-
-	}
+		
+	};
+	
+	
 	public boolean validatePassword(String passWord) throws UserValidatorException
 	{
 		try
